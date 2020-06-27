de1842bb4d8334c76ae256a8c92170522f9d7623
Fipeman <fsilvagalleguillos@gmail.com>
2020-06-27 12:36:14 -0400

    cambios casos

--- desafio3/caso1.sql
+++ desafio3/caso1.sql
@@ -2,4 +2,4 @@ CREATE TABLE departamento(id SERIAL, nombre_depto VARCHAR(100), PRIMARY KEY (id)
 
 CREATE TABLE trabajador(rut VARCHAR(15), nombre VARCHAR(50), dirección VARCHAR(200), id_depto SERIAL, PRIMARY KEY (rut), FOREIGN KEY (id_depto) REFERENCES departamento(id)) ;
 
-CREATE TABLE liquidacion(id_liq SERIAL, rut_liq VARCHAR(15), liquidacion_monto INT, PRIMARY KEY (id_liq), FOREIGN KEY (rut_liq) REFERENCES trabajador(rut));
+CREATE TABLE liquidacion(rut_liq VARCHAR(15), liquidacion_monto INT, FOREIGN KEY (rut_liq) REFERENCES trabajador(rut));
